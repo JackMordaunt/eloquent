c72d46ddf07a002855179f8a28c93f78258a4500
Jack Mordaunt <jackmordaunt@gmail.com>
2021-06-05 07:29:03 +0800

    wip: sketch
    Signed-off-by: Jack Mordaunt <jackmordaunt@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+)
 
+func main() {
+	var (
+		g Generator
+	)
+	if err := func() error {
+		f, err := parser.ParseFile(nil, "", nil, parser.ParseComments)
+		if err != nil {
+			return fmt.Errorf("parsing file: %w", err)
+		}
+		ast.Walk(g, f)
+		return nil
+	}(); err != nil {
+		fmt.Printf("error: %v", err)
+	}
+}
+
+// Generator visits ast nodes and creates a fluent method for each public struct
+// field.
+type Generator struct {
+}
+
+func (g Generator) Visit(n ast.Node) ast.Visitor {
+	return g
 }
