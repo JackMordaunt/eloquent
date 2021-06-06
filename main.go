e653e6e874f50ce313ed11b463f98f6f71901245
Jack Mordaunt <jackmordaunt@gmail.com>
2021-06-06 17:24:33 +0800

    feat: generate fluent methods for a directory
    If output file not specified, writes to stdout.
    
    Note: only works for primitive field types, not structs, pointers, slices or maps (yet).
    Signed-off-by: Jack Mordaunt <jackmordaunt@gmail.com>

--- main.go
+++ main.go
@@ -8,104 +8,130 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
-	"sync"
 	"text/template"
 	"unicode"
-
-	"github.com/davecgh/go-spew/spew"
 )
-
-func init() {
-	spew.Config = spew.ConfigState{
-		Indent:            "    ",
-		DisableMethods:    true,
-		DisableCapacities: true,
-	}
-}
 
 func main() {
 	if err := func() error {
 		var (
+			// path to directory containing go files.
 			path string
+			// out path to generated file eg "fluent.go".
+			out string
+			// w is the output writer. Points to stdout when no output file is specified.
+			w io.Writer
 		)
 		if len(os.Args) > 1 {
 			path = os.Args[1]
 		}
+		if len(os.Args) > 2 {
+			out = os.Args[2]
+		}
 		if path == "" {
-			return fmt.Errorf("specify package path")
+			return fmt.Errorf("specify directory path")
 		}
-		src, err := ioutil.ReadFile(path)
+		if out == "" {
+			w = os.Stdout
+		} else {
+			outf, err := os.Create(out)
+			if err != nil {
+				return fmt.Errorf("creating output file: %w", err)
+			}
+			defer outf.Close()
+			w = outf
+		}
+		// Note(jfm): Should we assume the directory matches the package name? This is common
+		// convention, but is not guaranteed.
+		fmt.Fprintf(w, "package %s\n\n", strings.Split(filepath.Base(path), ".")[0])
+		entries, err := ioutil.ReadDir(path)
 		if err != nil {
-			return fmt.Errorf("reading file: %w", err)
+			return fmt.Errorf("reading dir: %w", err)
 		}
-		out, err := Generate(string(src))
-		if err != nil {
-			return fmt.Errorf("generating fluent methods: %w", err)
+		for _, entry := range entries {
+			if entry.IsDir() {
+				continue
+			}
+			if filepath.Ext(entry.Name()) != ".go" {
+				continue
+			}
+			if strings.Contains(entry.Name(), "fluent") {
+				continue
+			}
+			if err := func() error {
+				srcf, err := os.Open(filepath.Join(path, entry.Name()))
+				if err != nil {
+					return fmt.Errorf("reading file: %w", err)
+				}
+				defer srcf.Close()
+				fragments, err := Process(srcf)
+				if err != nil {
+					return fmt.Errorf("generating fluent methods: %w", err)
+				}
+				for _, fragment := range fragments {
+					if _, err := fmt.Fprint(w, fragment); err != nil {
+						return fmt.Errorf("writing source code fragments to file: %w", err)
+					}
+				}
+				return nil
+			}(); err != nil {
+				return err
+			}
 		}
-		return ioutil.WriteFile(fmt.Sprintf("%s_fluent.go", path), []byte(out), 0665)
+		return nil
 	}(); err != nil {
 		fmt.Printf("error: %v", err)
 	}
 }
 
-// Generate fluent methods for struct definitions in the given source.
-func Generate(src string) (string, error) {
-	fs := token.NewFileSet()
-	f, err := parser.ParseFile(fs, "", src, parser.ParseComments)
-	if err != nil {
-		return "", fmt.Errorf("parsing file: %w", err)
-	}
-	var b strings.Builder
-	ast.Walk(StructVisitor{Suffix: "Style", Wr: &b}, f)
-	return b.String(), nil
-}
-
-// Setter generates a fluent method.
-type Setter struct {
-	StructType string
-	FieldName  string
-	FieldType  string
-	Receiver   string
-	Argument   string
-	Doc        string
-
-	init sync.Once
-	t    *template.Template
-	err  error
-}
-
-func (s *Setter) String() string {
-	s.init.Do(func() {
-		s.t, s.err = template.New("").Parse(strings.TrimSpace(`
-// {{.Doc}}
-func ({{.Receiver}} {{.StructType}}) With{{.FieldName}}({{.Argument}} {{.FieldType}}) {{.StructType}} {
-	{{.Receiver}}.{{.FieldName}} = {{.Argument}}
+// MethodTemplate used to generate fluent setter methods.
+var MethodTemplate = template.Must(template.New("").Parse(strings.TrimSpace(`
+{{.Doc}}
+func ({{.Receiver}} {{.StructType}}) With{{.FieldIdent}}({{.Argument}} {{.FieldType}}) {{.StructType}} {
+	{{.Receiver}}.{{.FieldIdent}} = {{.Argument}}
 	return {{.Receiver}}
-}
-		`))
-	})
-	if s.t == nil {
-		return ""
+}`)))
+
+// Process parses source code from `r` and returns source code fragments implementing fluent setter
+// methods for each viable struct definition.
+func Process(r io.Reader) (list []string, err error) {
+	src, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
 	}
-	b := strings.Builder{}
-	s.err = s.t.Execute(&b, s)
-	return b.String()
+	f, err := parser.ParseFile(token.NewFileSet(), "", src, parser.ParseComments)
+	if err != nil {
+		return nil, fmt.Errorf("parsing file: %w", err)
+	}
+	ast.Walk(StructVisitor{Suffix: "Style", Callback: func(s Struct) {
+		if err != nil {
+			return
+		}
+		fragment, e := s.GenerateFluentMethods()
+		if e != nil {
+			err = fmt.Errorf("generating method for struct %q: %w", s.Type, e)
+		}
+		if fragment != "" {
+			list = append(list, fragment)
+		}
+	}}, f)
+	return list, err
 }
 
-func (s *Setter) Err() error {
-	return s.err
-}
-
-// StructVisitor visits struct definitions and generates Fluent method stubs.
+// StructVisitor visits struct definitions on an ast.
 type StructVisitor struct {
 	// Suffix to match against. Could use regex for more generic approach.
 	Suffix string
-	// Wr to write generated methods into. Typically a file handle.
-	Wr io.Writer
+	// Callback is invoked for each struct definition.
+	Callback func(Struct)
 }
 
 func (sv StructVisitor) Visit(n ast.Node) ast.Visitor {
+	if sv.Callback == nil {
+		return nil
+	}
 	t, ok := n.(*ast.TypeSpec)
 	if !ok {
 		return sv
@@ -117,28 +143,73 @@ func (sv StructVisitor) Visit(n ast.Node) ast.Visitor {
 	if !ok {
 		return sv
 	}
+	var fields []Field
 	for _, field := range obj.Fields.List {
 		if isEmbedded := len(field.Names) == 0; isEmbedded {
 			continue
 		}
-		var (
-			structType   = t.Name.String()
-			fieldName    = field.Names[0].String()
-			fieldType    = fmt.Sprintf("%s", field.Type)
-			fieldComment = strings.TrimSpace(field.Doc.Text())
-		)
-		if isExported := unicode.IsUpper(rune(fieldName[0])); !isExported {
+		ident := field.Names[0].String()
+		if isExported := unicode.IsUpper(rune(ident[0])); !isExported {
 			continue
 		}
-		fmt.Fprint(sv.Wr, &Setter{
-			StructType: structType,
-			FieldName:  fieldName,
-			FieldType:  fieldType,
-			Receiver:   "style",
-			Argument:   string(strings.ToLower(fieldType)[0]),
-			Doc:        fmt.Sprintf("With%s", fieldComment),
+		fmt.Printf("%+v\n", field.Type)
+		// TODO(jfm): handle non-primitive types: structures, pointers, slices and maps.
+		fields = append(fields, Field{
+			Type:       fmt.Sprintf("%s", field.Type),
+			Identifer:  ident,
+			DocComment: strings.TrimSpace(field.Doc.Text()),
 		})
-		fmt.Fprint(sv.Wr, "\n\n")
 	}
+	sv.Callback(Struct{
+		Type:   t.Name.String(),
+		Fields: fields,
+	})
 	return sv
+}
+
+// Struct specifies a struct definition.
+type Struct struct {
+	Type   string
+	Fields []Field
+}
+
+// Field specifies a struct field definition.
+type Field struct {
+	Type       string
+	Identifer  string
+	DocComment string
+}
+
+// GenerateFluentMethods produces source code fragments that implement fluent setter methods for
+// the struct definition.
+func (s Struct) GenerateFluentMethods() (string, error) {
+	var b strings.Builder
+	for _, field := range s.Fields {
+		if err := MethodTemplate.Execute(&b, struct {
+			StructType string
+			FieldType  string
+			FieldIdent string
+			Receiver   string
+			Argument   string
+			Doc        string
+		}{
+			StructType: s.Type,
+			FieldType:  field.Type,
+			FieldIdent: field.Identifer,
+			Receiver:   "style",
+			Argument:   strings.ToLower(string(field.Type[0])),
+			Doc: func() string {
+				if field.DocComment != "" {
+					return field.DocComment
+				}
+				return ""
+			}(),
+		}); err != nil {
+			return "", fmt.Errorf("executing template: %w", err)
+		}
+		if _, err := fmt.Fprint(&b, "\n"); err != nil {
+			return "", fmt.Errorf("adding spacer: %w", err)
+		}
+	}
+	return b.String(), nil
 }
